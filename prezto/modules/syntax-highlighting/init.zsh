d9011ac4bf1a13b04127f14968a6c248681af54a
qdu <q.du@connect.ust.hk>
2019-03-19 22:18:15 +0800

    misc

--- prezto/modules/syntax-highlighting/init.zsh
+++ prezto/modules/syntax-highlighting/init.zsh
@@ -13,24 +13,7 @@ fi
 # Source module files.
 source "${0:h}/external/fast-syntax-highlighting.plugin.zsh" || return 1
 
-# Set highlighters.
-zstyle -a ':prezto:module:syntax-highlighting' highlighters 'ZSH_HIGHLIGHT_HIGHLIGHTERS'
-if (( ${#ZSH_HIGHLIGHT_HIGHLIGHTERS[@]} == 0 )); then
-  ZSH_HIGHLIGHT_HIGHLIGHTERS=(main)
+if zstyle -t ':prezto:module:syntax-highlighting' theme; then
+    echo $theme
+    fast-theme $theme
 fi
-
-# Set highlighting styles.
-typeset -A syntax_highlighting_styles
-zstyle -a ':prezto:module:syntax-highlighting' styles 'syntax_highlighting_styles'
-for syntax_highlighting_style in "${(k)syntax_highlighting_styles[@]}"; do
-  ZSH_HIGHLIGHT_STYLES[$syntax_highlighting_style]="$syntax_highlighting_styles[$syntax_highlighting_style]"
-done
-unset syntax_highlighting_style{s,}
-
-# Set pattern highlighting styles.
-typeset -A syntax_pattern_styles
-zstyle -a ':prezto:module:syntax-highlighting' pattern 'syntax_pattern_styles'
-for syntax_pattern_style in "${(k)syntax_pattern_styles[@]}"; do
-  ZSH_HIGHLIGHT_PATTERNS[$syntax_pattern_style]="$syntax_pattern_styles[$syntax_pattern_style]"
-done
-unset syntax_pattern_style{s,}
