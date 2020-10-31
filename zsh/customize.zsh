47bed5489ba35b387fe32dbc24af91007331ef97
qdu <q.du@connect.ust.hk>
2020-10-31 17:19:41 +0800

    Add alias ps-grep and gpl

--- zsh/customize.zsh
+++ zsh/customize.zsh
@@ -19,7 +19,7 @@ done
 
 # git alias
 _git_log_medium_format='%C(bold)Commit:%C(reset) %C(green)%H%C(red)%d%n%C(bold)Author:%C(reset) %C(cyan)%an <%ae>%n%C(bold)Date:%C(reset)   %C(blue)%ai (%ar)%C(reset)%n%+B'
-alias gpl='git pull'
+alias gpl='git pull --rebase'
 alias gl='git log --topo-order --pretty=format:"${_git_log_medium_format}"'
 alias glg='git log --topo-order --stat --pretty=format:"${_git_log_medium_format}"'
 alias gst='git status -s'
