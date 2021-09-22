3cb96f70724b4088eec6c05e4372ab4ed4e4a3d8
qdu <q.du@connect.ust.hk>
2021-09-22 16:16:55 +0800

    Remove unused lualine configs

--- vim/nvim/plugs.lua
+++ vim/nvim/plugs.lua
@@ -1,5 +1,5 @@
---- Check if a file or directory exists in this path
 function Exists(file)
+    -- Check if a file or directory exists in this path
     local ok, _, code = os.rename(file, file)
     if not ok then
         if code == 13 then
@@ -10,73 +10,16 @@ function Exists(file)
     return ok
 end
 
---- Check if a directory exists in this path
 function IsDir(path)
-    -- "/" works on both Unix and Windows
-    return Exists(path.."/")
+    -- Check if a directory exists in this path
+    return Exists(path.."/") -- "/" works on both Unix and Windows
 end
 
---- lualine
-local HOME = os.getenv("HOME")
-if Exists(HOME .. "/.vim/bundle/lualine.nvim/") then
-    require'lualine'.setup {
-        options = {
-            icons_enabled = true,
-            theme = 'gruvbox_light',
-            component_separators = '',
-            section_separators = '',
-            disabled_filetypes = {}
-        },
-        sections = {
-            lualine_a = {'mode'},
-            lualine_b = {
-                {
-                    'diff',
-                    colored = true, -- displays diff status in color if set to true
-                    -- all colors are in format #rrggbb
-                    color_added = nil, -- changes diff's added foreground color
-                    color_modified = nil, -- changes diff's modified foreground color
-                    color_removed = nil, -- changes diff's removed foreground color
-                    symbols = {added = '+', modified = '~', removed = '-'} -- changes diff symbols
-                },
-                'branch'
-            },
-            lualine_c = {{'filename', path = 1}},
-            lualine_x = {'fileformat'},
-            lualine_y = {'filetype'},
-            lualine_z = {
-                'progress',
-                "GetStatusLineInfo()",
-                {
-                    'diagnostics',
-                    -- table of diagnostic sources, available sources:
-                    -- nvim_lsp, coc, ale, vim_lsp
-                    sources = {'coc', 'ale'},
-                    -- displays diagnostics from defined severity
-                    sections = {'error', 'warn', 'info', 'hint'},
-                    -- all colors are in format #rrggbb
-                    color_error = nil, -- changes diagnostic's error foreground color
-                    color_warn = nil, -- changes diagnostic's warn foreground color
-                    color_info = nil, -- Changes diagnostic's info foreground color
-                    color_hint = nil, -- Changes diagnostic's hint foreground color
-                    symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'}
-                }
-            }
-        },
-        inactive_sections = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = {'filename'},
-            lualine_x = {'location'},
-            lualine_y = {},
-            lualine_z = {}
-        },
-        tabline = {},
-        extensions = {'fugitive'}
-    }
-end
+local BUNDLE_DIR = os.getenv("HOME") .. "/.vim/bundle/"
 
-if Exists(HOME .. "/.vim/bundle/bufferline.nvim") then
+--- Configure plugs ---
+
+if Exists(BUNDLE_DIR .. "bufferline.nvim") then
     vim.opt.termguicolors = true
     require('bufferline').setup {
         options = {
@@ -131,18 +74,32 @@ if Exists(HOME .. "/.vim/bundle/bufferline.nvim") then
     ]])
 end
 
-if Exists(HOME .. "/.vim/bundle/nvim-treesitter") then
+if Exists(BUNDLE_DIR .. "nvim-treesitter") then
     require'nvim-treesitter.configs'.setup {
-      ensure_installed = { "c_sharp" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-      ignore_install = { }, -- List of parsers to ignore installing
-      highlight = {
+        ensure_installed = { "c_sharp", "vim" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+        ignore_install = { }, -- List of parsers to ignore installing
+        highlight = {
         enable = true,              -- false will disable the whole extension
-        disable = { "c" },  -- list of language that will be disabled
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-      },
+            disable = { "c" },  -- list of language that will be disabled
+            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+            -- Using this option may slow down your editor, and you may see some duplicate highlights.
+            -- Instead of true it can also be a list of languages
+            additional_vim_regex_highlighting = false,
+        },
+        indent = {
+            disable = {},
+            enable = true,
+        },
+        incremental_selection = {
+            disable = {},
+            enable = true,
+            keymaps = {
+                init_selection = "vv",
+                node_decremental = "vd",
+                node_incremental = "vn",
+                scope_incremental = "vs"
+            },
+        },
     }
 end
