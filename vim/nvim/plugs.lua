4475d200bbac2aa658e7daf0d9efc915a18e2dbb
qdu <q.du@connect.ust.hk>
2023-12-13 15:04:14 +0800

    Use the native nvim-lsp for csharp_ls

--- vim/nvim/plugs.lua
+++ vim/nvim/plugs.lua
@@ -17,9 +17,166 @@ end
 
 local BUNDLE_DIR = os.getenv("HOME") .. "/.vim/bundle/"
 local map = vim.api.nvim_set_keymap
-local set_hl = function(name, fg, bg) vim.api.nvim_set_hl(0, name, { foreground = fg, background = bg }) end
+local set_hl = function(name, fg, bg)
+    local settings = {}
+    if fg then settings.foreground = fg end
+    if bg then settings.background = bg end
+    vim.api.nvim_set_hl(0, name, settings)
+end
 local set_hl_link = function(name, linkto) vim.api.nvim_set_hl(0, name, { link = linkto, default = false }) end
 local get_hl = function(name) return vim.api.nvim_get_hl_by_name(name, true) end
+
+--- Configure LSP ---
+require 'lspconfig'.csharp_ls.setup {
+    cmd = { 'csharp-ls', '--loglevel', 'log' },
+    filetypes = { "cs" },
+    init_options = {
+        AutomaticWorkspaceInit = true
+    },
+    root_dir = function(_, _)
+        return vim.fs.dirname(
+            vim.fs.find(
+                function(name)
+                    return name:match('.*%.sln$')
+                end,
+                { upward = true, limit = math.huge, type = 'file' }
+            )[1]
+        )
+    end,
+    single_file_support = true,
+}
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', { clear = true }),
+    pattern = "*.cs",
+    callback = function(args)
+        vim.api.nvim_del_augroup_by_id(vim.api.nvim_create_augroup('coc_user_defined', { clear = true }))
+        vim.cmd([[ CocDisable ]])
+
+        -- Keymappings
+        local opts = { buffer = args.buf }
+        vim.keymap.set('n', '<leader>ho', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', '<leader>fo', function() vim.lsp.buf.format { async = true } end, opts)
+        vim.keymap.set('n', '<leader>fi', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', '<leader>dl', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', '<leader>re', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<leader>im', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', '<leader>da', vim.diagnostic.setloclist, { noremap = true, silent = true })
+        vim.keymap.set(
+            'n', '<C-j>',
+            function() if vim.fn.ListVisible() == 0 then vim.diagnostic.goto_next() else vim.cmd('cnext') end end,
+            { noremap = true, silent = true }
+        )
+        vim.keymap.set(
+            'n', '<C-k>',
+            function() if vim.fn.ListVisible() == 0 then vim.diagnostic.goto_prev() else vim.cmd('cprevious') end end,
+            { noremap = true, silent = true }
+        )
+
+        -- Diagnostics
+        local signs = {
+            { name = "DiagnosticSignError", text = "✗", severity = vim.diagnostic.severity.ERROR },
+            { name = "DiagnosticSignWarn",  text = "▲", severity = vim.diagnostic.severity.WARN },
+            { name = "DiagnosticSignHint",  text = "◇", severity = vim.diagnostic.severity.HINT },
+            { name = "DiagnosticSignInfo",  text = "●", severity = vim.diagnostic.severity.INFO },
+        }
+
+        for _, sign in ipairs(signs) do
+            vim.fn.sign_define(sign.name, { text = sign.text, texthl = sign.name, numhl = sign.name })
+        end
+
+        local function sign_mappings(name)
+            local ret = {}
+            for _, sign in ipairs(signs) do
+                ret[sign.severity] = sign[name]
+            end
+            return ret
+        end
+
+        vim.diagnostic.config({
+            virtual_text = false,
+            signs = {
+                text = sign_mappings("text"),
+                numhl = sign_mappings("name"),
+            },
+            underline = true,
+            update_in_insert = false,
+            severity_sort = true,
+            float = {
+                scope = "line",
+                focusable = true,
+                source = "always",
+                border = nil,
+                header = "",
+                prefix = "",
+                max_width = 90,
+                wrap = true
+            },
+        })
+
+        set_hl_link("NormalFloat", "Fmenu")
+        set_hl("DiagnosticError", get_hl("GruvboxRed").foreground, nil)
+        set_hl("DiagnosticWarn", get_hl("GruvboxYellowSign").foreground, nil)
+        set_hl("DiagnosticHint", get_hl("GruvboxYellowSign").foreground, nil)
+        set_hl("DiagnosticInfo", get_hl("GruvboxAquaSign").foreground, nil)
+        set_hl("FloatBorder", get_hl("VertSplit").foreground, get_hl("Fmenu").background)
+        set_hl_link("@lsp.type.namespace", "GruvboxAqua")
+        set_hl_link("DiagnosticFloatingError", "DiagnosticError")
+        set_hl_link("DiagnosticFloatingWarn", "DiagnosticWarn")
+        set_hl_link("DiagnosticFloatingHint", "DiagnosticHint")
+        set_hl_link("DiagnosticFloatingInfo", "DiagnosticInfo")
+
+        -- Signatures
+        vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+            vim.lsp.handlers.signature_help, {
+                border = nil,
+                max_width = 90,
+                focusable = false,
+            }
+        )
+        vim.api.nvim_create_autocmd({ "CursorHoldI" }, {
+            group = vim.api.nvim_create_augroup("CsLspSignature", {}),
+            pattern = "*.cs",
+            callback = function(_) vim.lsp.buf.signature_help() end
+        })
+
+        -- Completion
+        vim.g['deoplete#lsp#handler_enabled'] = true
+        vim.g['deoplete#lsp#use_icons_for_candidates'] = true
+        vim.fn['deoplete#custom#option']({
+            num_processes = -1,
+        })
+        vim.fn['deoplete#custom#source']('_', {
+            min_pattern_length = 1,
+            smart_case = true,
+            show_docstring = 1,
+        })
+        vim.fn['deoplete#custom#source']('lsp', {
+            show_docstring = 1,
+        })
+        vim.fn['deoplete#enable']()
+        vim.fn['deoplete#lsp#enable']()
+        local o = { noremap = true, silent = true, expr = true }
+        map('i', '<CR>', 'pumvisible() ? deoplete#close_popup() : "\\<CR>"', o)
+        map('i', '<C-d>', 'pumvisible() ? "\\<PageDown>" : "\\<C-d>"', o)
+        map('i', '<C-u>', 'pumvisible() ? "\\<PageUp>" : "\\<C-u>"', o)
+        map('i', '<C-space>', 'deoplete#manual_complete()', o)
+    end
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+    group = vim.api.nvim_create_augroup("CsLspHighlight", {}),
+    pattern = "cs",
+    callback = function(_)
+        set_hl_link("csStorage", "GruvboxRed")
+        set_hl_link("csClass", "GruvboxRed")
+        set_hl_link("csType", "GruvboxRed")
+        set_hl_link("csAccessModifier", "GruvboxRed")
+        set_hl_link("csInterpolationFormat", "String")
+        set_hl_link("Structure", "GruvboxYellow")
+    end
+})
 
 --- Configure plugs ---
 if Exists(BUNDLE_DIR .. "indent-blankline.nvim") then
@@ -148,11 +305,11 @@ end
 
 if Exists(BUNDLE_DIR .. "nvim-treesitter") then
     require 'nvim-treesitter.configs'.setup {
-        ensure_installed = { "vim", "python", "c_sharp" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-        ignore_install = {},                               -- List of parsers to ignore installing
+        ensure_installed = { "vim", "python" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+        ignore_install = {},                    -- List of parsers to ignore installing
         highlight = {
-            enable = true,                                 -- false will disable the whole extension
-            disable = {},                                  -- list of language that will be disabled
+            enable = true,                      -- false will disable the whole extension
+            disable = {},                       -- list of language that will be disabled
             additional_vim_regex_highlighting = false,
         },
         indent = {
